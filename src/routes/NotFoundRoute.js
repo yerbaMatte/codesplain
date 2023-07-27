e454c5563fb3b3e42ff0fbe56d1fcd0bb6812815
Lakshan Costa <lakshanrcosta@gmail.com>
2023-07-27 13:48:38 +0300

    React escape for single quote added

--- src/routes/NotFoundRoute.js
+++ src/routes/NotFoundRoute.js
@@ -8,7 +8,7 @@ function NotFound() {
 
         <p className='text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl'>Uh-oh!</p>
 
-        <p className='mt-4 text-gray-500'>We can't find that page.</p>
+        <p className='mt-4 text-gray-500'>We can&apos;t find that page.</p>
 
         <Link
           to='/'
